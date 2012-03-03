9308837efd18ba2bcb3e5fa982d9a26ae0aceae3
Joshua Bell <inexorabletash@gmail.com>
2012-03-03 16:55:35 -0500

    Expose atob/btoa to any global context (window or worker)

--- polyfill.js
+++ polyfill.js
@@ -587,88 +587,6 @@ if ('window' in this && 'document' in this) {
     window.BlobBuilder = window.WebKitBlobBuilder || window.MozBlobBuilder;
   }
 
-  //
-  // Base64 utility methods (HTML5)
-  //
-  // TODO: Expose to any workers as well
-  (function () {
-    /*jslint plusplus: true, bitwise: true*/
-    var B64_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    window.atob = window.atob || function (input) {
-      input = String(input);
-      var position = 0,
-          output = [],
-          buffer = 0, bits = 0, n;
-
-      input = input.replace(/\s/g, '');
-      if ((input.length % 4) === 0) { input = input.replace(/=+$/, ''); }
-      if ((input.length % 4) === 1) { throw new Error("InvalidCharacterError"); }
-      if (/[^+/0-9A-Za-z]/.test(input)) { throw new Error("InvalidCharacterError"); }
-
-      while (position < input.length) {
-        n = B64_ALPHABET.indexOf(input.charAt(position));
-        buffer = (buffer << 6) | n;
-        bits += 6;
-
-        if (bits === 24) {
-          output.push(String.fromCharCode((buffer >> 16) & 0xFF));
-          output.push(String.fromCharCode((buffer >>  8) & 0xFF));
-          output.push(String.fromCharCode(buffer & 0xFF));
-          bits = 0;
-          buffer = 0;
-        }
-        position += 1;
-      }
-
-      if (bits === 12) {
-        buffer = buffer >> 4;
-        output.push(String.fromCharCode(buffer & 0xFF));
-      } else if (bits === 18) {
-        buffer = buffer >> 2;
-        output.push(String.fromCharCode((buffer >> 8) & 0xFF));
-        output.push(String.fromCharCode(buffer & 0xFF));
-      }
-
-      return output.join('');
-    };
-
-    window.btoa = window.btoa || function (input) {
-      input = String(input);
-      var position = 0,
-          len = input.length,
-          out = [],
-          o1, o2, o3,
-          e1, e2, e3, e4;
-
-      if (/[^\x00-\xFF]/.test(input)) { throw new Error("InvalidCharacterError"); }
-
-      while (position < len) {
-        o1 = input.charCodeAt(position++);
-        o2 = input.charCodeAt(position++);
-        o3 = input.charCodeAt(position++);
-
-        // 111111 112222 222233 333333
-        e1 = o1 >> 2;
-        e2 = ((o1 & 0x3) << 4) | (o2 >> 4);
-        e3 = ((o2 & 0xf) << 2) | (o3 >> 6);
-        e4 = o3 & 0x3f;
-
-        if (position === len + 2) {
-          e3 = 64; e4 = 64;
-        }
-        else if (position === len + 1) {
-          e4 = 64;
-        }
-
-        out.push(B64_ALPHABET.charAt(e1),
-                 B64_ALPHABET.charAt(e2),
-                 B64_ALPHABET.charAt(e3),
-                 B64_ALPHABET.charAt(e4));
-      }
-
-      return out.join('');
-    };
-  } ());
 
   //----------------------------------------------------------------------
   //
@@ -951,3 +869,84 @@ if ('window' in this && 'document' in this) {
   }());
 }
 
+//
+// Base64 utility methods (HTML5)
+//
+(function (global) {
+  /*jslint plusplus: true, bitwise: true*/
+  var B64_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  global.atob = global.atob || function (input) {
+    input = String(input);
+    var position = 0,
+        output = [],
+        buffer = 0, bits = 0, n;
+
+    input = input.replace(/\s/g, '');
+    if ((input.length % 4) === 0) { input = input.replace(/=+$/, ''); }
+    if ((input.length % 4) === 1) { throw new Error("InvalidCharacterError"); }
+    if (/[^+/0-9A-Za-z]/.test(input)) { throw new Error("InvalidCharacterError"); }
+
+    while (position < input.length) {
+      n = B64_ALPHABET.indexOf(input.charAt(position));
+      buffer = (buffer << 6) | n;
+      bits += 6;
+
+      if (bits === 24) {
+        output.push(String.fromCharCode((buffer >> 16) & 0xFF));
+        output.push(String.fromCharCode((buffer >>  8) & 0xFF));
+        output.push(String.fromCharCode(buffer & 0xFF));
+        bits = 0;
+        buffer = 0;
+      }
+      position += 1;
+    }
+
+    if (bits === 12) {
+      buffer = buffer >> 4;
+      output.push(String.fromCharCode(buffer & 0xFF));
+    } else if (bits === 18) {
+      buffer = buffer >> 2;
+      output.push(String.fromCharCode((buffer >> 8) & 0xFF));
+      output.push(String.fromCharCode(buffer & 0xFF));
+    }
+
+    return output.join('');
+  };
+
+  global.btoa = global.btoa || function (input) {
+    input = String(input);
+    var position = 0,
+        len = input.length,
+        out = [],
+        o1, o2, o3,
+        e1, e2, e3, e4;
+
+    if (/[^\x00-\xFF]/.test(input)) { throw new Error("InvalidCharacterError"); }
+
+    while (position < len) {
+      o1 = input.charCodeAt(position++);
+      o2 = input.charCodeAt(position++);
+      o3 = input.charCodeAt(position++);
+
+      // 111111 112222 222233 333333
+      e1 = o1 >> 2;
+      e2 = ((o1 & 0x3) << 4) | (o2 >> 4);
+      e3 = ((o2 & 0xf) << 2) | (o3 >> 6);
+      e4 = o3 & 0x3f;
+
+      if (position === len + 2) {
+        e3 = 64; e4 = 64;
+      }
+      else if (position === len + 1) {
+        e4 = 64;
+      }
+
+      out.push(B64_ALPHABET.charAt(e1),
+               B64_ALPHABET.charAt(e2),
+               B64_ALPHABET.charAt(e3),
+               B64_ALPHABET.charAt(e4));
+    }
+
+    return out.join('');
+  };
+} (this));
