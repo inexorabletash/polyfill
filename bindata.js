6de6d92796ad4765d9ce2e9099744f67bbd7f41e
Joshua Bell <inexorabletash@gmail.com>
2012-12-16 02:29:47 -0500

    Redo binary data with block semantics

--- bindata.js
+++ bindata.js
@@ -1,6 +1,8 @@
 // http://wiki.ecmascript.org/doku.php?id=harmony:binary_data
 (function(global){
 
+  var endianness = true;
+
   (function() {
     var orig = Object.prototype.toString;
     Object.prototype.toString = function toString() {
@@ -19,131 +21,92 @@
   // TODO: .update()
   // TODO: read/write into ArrayBuffer
   // TODO: int64/uint64
-  // TODO: bytes vs. byteLength
   // TODO: __Value__ as ArrayBufferView?
+
+  // "block" is defined as an object with a __View__ property (which is an ArrayBufferView)
+  function isBlockObject(v) { return '__View__' in Object(v); }
+  function isBlockType(t) { return 'bytes' in t && '__Convert__' in t && '__Reify__' in t; }
 
   // Intrinsic types
   [
-    {name: 'int8', getter: 'getInt8', setter: 'setInt8', byteLength: 1 },
-    {name: 'int16', getter: 'getInt16', setter: 'setInt16', byteLength: 2 },
-    {name: 'int32', getter: 'getInt32', setter: 'setInt32', byteLength: 4 },
-    {name: 'uint8', getter: 'getUint8', setter: 'setUint8', byteLength: 1 },
-    {name: 'uint16', getter: 'getUint16', setter: 'setUint16', byteLength: 2 },
-    {name: 'uint32', getter: 'getUint32', setter: 'setUint32',byteLength: 4 },
-    {name: 'float32', getter: 'getFloat32', setter: 'setFloat32', byteLength: 4 },
-    {name: 'float64', getter: 'getFloat64', setter: 'setFloat64', byteLength: 8 }
+    {name: 'int8', getter: 'getInt8', setter: 'setInt8', bytes: 1 },
+    {name: 'int16', getter: 'getInt16', setter: 'setInt16', bytes: 2 },
+    {name: 'int32', getter: 'getInt32', setter: 'setInt32', bytes: 4 },
+    {name: 'uint8', getter: 'getUint8', setter: 'setUint8', bytes: 1 },
+    {name: 'uint16', getter: 'getUint16', setter: 'setUint16', bytes: 2 },
+    {name: 'uint32', getter: 'getUint32', setter: 'setUint32',bytes: 4 },
+    {name: 'float32', getter: 'getFloat32', setter: 'setFloat32', bytes: 4 },
+    {name: 'float64', getter: 'getFloat64', setter: 'setFloat64', bytes: 8 }
   ].forEach(function(desc) {
     var proto = {};
 
     var t = function SomeNumericType(value) {
-      this.__Value__ = new Uint8Array(desc.byteLength).buffer;
-      if (value !== (void 0)) {
-        t.__Convert__(value, this.__Value__, 0);
-      }
+      if (this instanceof SomeNumericType) { throw new TypeError("Numeric types should have value semantics"); }
+      value = Number(value);
+      return t.__Convert__(value);
     };
-    t.__Convert__ = function Convert(value, buffer, offset) {
-      (new DataView(buffer, offset))[desc.setter](0, value, true);
+    t.__Construct__ = function Construct() {
+      var block = Object.create(proto);
+      block.__View__ = new Uint8Array(desc.bytes);
+      return block;
     };
-    t.__Reify__ = function Reify(buffer, offset) {
-      return (new DataView(buffer, offset))[desc.getter](0, true);
+    t.__Convert__ = function Convert(value) {
+      var block = t.__Construct__();
+      (new DataView(block.__View__.buffer))[desc.setter](0, value, endianness);
+      return block;
+    };
+    t.__Reify__ = function Reify(block) {
+      var view = block.__View__;
+      return (new DataView(view.buffer, view.byteOffset, view.byteLength))[desc.getter](0, endianness);
     };
 
     t.prototype = proto;
     t.__Class__ = 'DataType';
     t.__DataType__ = desc.name;
-    t.bytes = t.byteLength = desc.byteLength;
+    t.bytes = desc.bytes;
     t.prototype.valueOf = function valueOf() {
-      return t.__Reify__(this.__Value__, 0);
+      return t.__Reify__(this);
     };
 
     global[desc.name] = t;
   });
 
-  function StructType(fields) {
-
-    var proto = {};
-    var byteLength = 0;
-
-    Object.keys(fields).forEach(function(name) {
-      var type = fields[name];
-      if (!('byteLength' in type && '__Convert__' in type && '__Reify__' in type)) {
-        throw new TypeError();
-      }
-
-      (function(name, type, offset){
-        Object.defineProperty(proto, name, {
-          get: function() {
-            return type.__Reify__(this.__Value__, offset);
-          },
-          set: function(value) {
-            type.__Convert__(value, this.__Value__, offset);
-          }
-        });
-      }(name, type, byteLength));
-      byteLength += type.byteLength;
-    });
-
-    var t = function SomeStructType(value) {
-      return t.__Construct__(value, this);
-    };
-    t.__Construct__ = function Construct(val, s) {
-      s.__Value__ = new Uint8Array(byteLength).buffer;
-      s.__Class__ = 'Block';
-      s.__DataType__ = t;
-      if (val !== (void 0)) {
-        t.__Convert__(val, s.__Value__, 0);
-      }
-    };
-    t.__Convert__ = function Convert(value, buffer, offset) {
-      Object.keys(fields).forEach(function(name) {
-        var type = fields[name];
-        type.__Convert__(value[name], buffer, offset);
-        offset += type.byteLength;
-      });
-    };
-    t.__Reify__ = function Reify(buffer, offset) {
-      var result = {};
-      Object.keys(fields).forEach(function(name) {
-        var type = fields[name];
-        result[name] = type.__Reify__(buffer, offset);
-        offset += type.byteLength;
-      });
-      return result;
-    };
-
-    t.prototype = proto;
-    t.__Class__ = 'DataType';
-    t.__DataType__ = 'struct';
-    t.prototype.constructor = t;
-    t.fields = fields; // TODO: copy/freeze
-    t.bytes = t.byteLength = byteLength;
-    t.prototype.valueOf = function valueOf() {
-      return t.__Reify__(this.__Value__, 0);
-    };
-
-    return t;
+  function viewCopy(src, dst, offset, length) {
+    var srcv = new Uint8Array(src.buffer, src.byteOffset, length);
+    var dstv = new Uint8Array(dst.buffer, dst.byteOffset + offset, length);
+    for (var i = 0; i < length; ++i) {
+      dstv[i] = srcv[i];
+    }
   }
-
 
   function ArrayType(type, length) {
     var proto = {};
     length = length | 0;
 
-    if (!('byteLength' in type && '__Convert__' in type && '__Reify__' in type)) {
-      throw new TypeError();
-    }
+    if (!isBlockType(type)) { throw new TypeError("Type is not a block type"); }
 
-    var byteLength = type.byteLength * length;
+    var bytes = type.bytes * length;
+
+    function getter(thisobj, index) {
+      var view = thisobj.__View__;
+      var offset = type.bytes * index;
+      return type.__Reify__({__View__: new Uint8Array(view.buffer, view.byteOffset + offset, type.bytes)});
+    }
+    function setter(thisobj, index, value) {
+      var src = type.__Convert__(value).__View__;
+      var dst = thisobj.__View__;
+      viewCopy(src, dst, type.bytes * index, type.bytes);
+    }
 
     for (var i = 0; i < length; ++i) {
       (function(type, index) {
 
         Object.defineProperty(proto, index, {
           get: function() {
-            return type.__Reify__(this.__Value__, type.byteLength * index);
+            return getter(this, index);
           },
           set: function(value) {
-            type.__Convert__(value, this.__Value__, type.byteLength * index);
+            setter(this, index, value);
           }
         });
 
@@ -151,28 +114,28 @@
     }
 
     var t = function SomeArrayType(value) {
-      t.__Construct__(value, this);
+      if (!(this instanceof SomeArrayType)) { throw new TypeError("objects have reference semantics"); }
+      return t.__Convert__(value);
     };
-    t.__Construct__ = function Construct(val, a) {
-      a.__Class__ = 'Data';
-      a.__Value__ = new Uint8Array(byteLength).buffer;
-      a.__DataType__ = t;
-      a.length = length;
-      if (val !== (void 0)) {
-        t.__Convert__(val, a.__Value__, 0);
-      }
+    t.__Construct__ = function Construct() {
+      var block = Object.create(proto);
+      block.__Class__ = 'Data';
+      block.__View__ = new Uint8Array(bytes);
+      block.__DataType__ = t;
+      block.length = length;
+      return block;
     };
-    t.__Convert__ = function Convert(value, buffer, offset) {
+    t.__Convert__ = function Convert(value) {
+      var block = t.__Construct__();
       for (var i = 0; i < length; ++i) {
-        type.__Convert__(value[i], buffer, offset);
-        offset += type.byteLength;
+        setter(block, i, value[i]);
       };
+      return block;
     };
-    t.__Reify__ = function Reify(buffer, offset) {
+    t.__Reify__ = function Reify(block) {
       var result = [];
       for (var i = 0; i < length; ++i) {
-        result[i] = type.__Reify__(buffer, offset);
-        offset += type.byteLength;
+        result[i] = getter(block, i);
       }
       return result;
     };
@@ -184,21 +147,90 @@
     t.__ElementType__ = type;
     t.__Length__ = length;
     t.prototype.constructor = t;
-    t.prototype.fill = function fill(val) {
+    t.prototype.fill = function fill(value) {
       for (var i = 0; i < t.__Length__; ++i) {
-        type.__Convert__(val, this.__Value__, i * t.__ElementType__.bytes);
+        setter(this, i, value);
       }
     };
     t.elementType = type;
     t.length = length; // TODO: Fails because t is a Function
-    t.bytes = t.byteLength = byteLength;
+    t.bytes = bytes;
     t.prototype.valueOf = function valueOf() {
-      return t.__Reify__(this.__Value__, 0);
+      return t.__Reify__(this);
     };
 
     return t;
   }
 
-  global.StructType = StructType;
+  function StructType(fields) {
+
+    var proto = {};
+    var bytes = 0;
+
+    // TODO: Define getter/setter (need to compute per-field offset)
+    Object.keys(fields).forEach(function(name) {
+      var type = fields[name];
+      if (!isBlockType(type)) { throw new TypeError("Type for '" + name + "' is not a block type"); }
+
+      (function(name, type, offset){
+        Object.defineProperty(proto, name, {
+          get: function() {
+            var view = this.__View__;
+            return type.__Reify__({__View__: new Uint8Array(view.buffer, view.byteOffset + offset, type.bytes)});
+          },
+          set: function(value) {
+            var src = type.__Convert__(value).__View__;
+            var dst = this.__View__;
+            viewCopy(src, dst, offset, type.bytes);
+          }
+        });
+      }(name, type, bytes));
+      bytes += type.bytes;
+    });
+
+    var t = function SomeStructType(value) {
+      if (!(this instanceof SomeStructType)) { throw new TypeError("objects have reference semantics"); }
+      return t.__Convert__(value);
+    };
+    t.__Construct__ = function Construct() {
+      var block = Object.create(proto);
+      block.__View__ = new Uint8Array(bytes);
+      block.__Class__ = 'Block';
+      block.__DataType__ = t;
+      return block;
+    };
+    t.__Convert__ = function Convert(value) {
+      var block = t.__Construct__();
+      var offset = 0;
+      Object.keys(fields).forEach(function(name) {
+        block[name] = value[name];
+      });
+      return block;
+    };
+    t.__Reify__ = function Reify(block) {
+      var result = {};
+      var view = block.__View__;
+      Object.keys(fields).forEach(function(name) {
+        var type = fields[name];
+        result[name] = type.__Reify__({__View__: new Uint8Array(view.buffer, view.byteOffset + offset, type.bytes)});
+        offset += type.bytes;
+      });
+      return result;
+    };
+
+    t.prototype = proto;
+    t.__Class__ = 'DataType';
+    t.__DataType__ = 'struct';
+    t.prototype.constructor = t;
+    t.fields = fields; // TODO: copy/freeze
+    t.bytes = bytes;
+    t.prototype.valueOf = function valueOf() {
+      return t.__Reify__(this);
+    };
+
+    return t;
+  }
+
   global.ArrayType = ArrayType;
+  global.StructType = StructType;
 }(self));
