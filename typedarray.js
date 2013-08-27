dc4c1725f4334f9d939185987c37f5d0b76a6a1a
Joshua Bell <inexorabletash@gmail.com>
2013-08-26 20:05:02 -0700

    Snapshot Math functions

--- typedarray.js
+++ typedarray.js
@@ -61,6 +61,15 @@
     };
   }());
 
+  // Snapshot intrinsics
+  var LN2 = Math.LN2,
+      abs = Math.abs,
+      floor = Math.floor,
+      log = Math.log,
+      min = Math.min,
+      pow = Math.pow,
+      round = Math.round;
+
   // Create an INDEX_SIZE_ERR event - intentionally induces a DOM error if possible
   function new_INDEX_SIZE_ERR() {
     try {
@@ -150,7 +159,7 @@
   function packUint8(n) { return [n & 0xff]; }
   function unpackUint8(bytes) { return as_unsigned(bytes[0], 8); }
 
-  function packUint8Clamped(n) { n = Math.round(Number(n)); return [n < 0 ? 0 : n > 0xff ? 0xff : n & 0xff]; }
+  function packUint8Clamped(n) { n = round(Number(n)); return [n < 0 ? 0 : n > 0xff ? 0xff : n & 0xff]; }
 
   function packInt16(n) { return [(n >> 8) & 0xff, n & 0xff]; }
   function unpackInt16(bytes) { return as_signed(bytes[0] << 8 | bytes[1], 16); }
@@ -174,31 +183,31 @@
     if (v !== v) {
       // NaN
       // http://dev.w3.org/2006/webapi/WebIDL/#es-type-mapping
-      e = (1 << bias) - 1; f = Math.pow(2, fbits - 1); s = 0;
+      e = (1 << bias) - 1; f = pow(2, fbits - 1); s = 0;
     } else if (v === Infinity || v === -Infinity) {
       e = (1 << bias) - 1; f = 0; s = (v < 0) ? 1 : 0;
     } else if (v === 0) {
       e = 0; f = 0; s = (1 / v === -Infinity) ? 1 : 0;
     } else {
       s = v < 0;
-      v = Math.abs(v);
+      v = abs(v);
 
-      if (v >= Math.pow(2, 1 - bias)) {
+      if (v >= pow(2, 1 - bias)) {
         // Normalized
-        ln = Math.min(Math.floor(Math.log(v) / Math.LN2), bias);
+        ln = min(floor(log(v) / LN2), bias);
         e = ln + bias;
-        f = Math.round(v * Math.pow(2, fbits - ln) - Math.pow(2, fbits));
+        f = round(v * pow(2, fbits - ln) - pow(2, fbits));
       } else {
         // Denormalized
         e = 0;
-        f = Math.round(v / Math.pow(2, 1 - bias - fbits));
+        f = round(v / pow(2, 1 - bias - fbits));
       }
     }
 
     // Pack sign, exponent, fraction
     bits = [];
-    for (i = fbits; i; i -= 1) { bits.push(f % 2 ? 1 : 0); f = Math.floor(f / 2); }
-    for (i = ebits; i; i -= 1) { bits.push(e % 2 ? 1 : 0); e = Math.floor(e / 2); }
+    for (i = fbits; i; i -= 1) { bits.push(f % 2 ? 1 : 0); f = floor(f / 2); }
+    for (i = ebits; i; i -= 1) { bits.push(e % 2 ? 1 : 0); e = floor(e / 2); }
     bits.push(s ? 1 : 0);
     bits.reverse();
     str = bits.join('');
@@ -238,10 +247,10 @@
       return f !== 0 ? NaN : s * Infinity;
     } else if (e > 0) {
       // Normalized
-      return s * Math.pow(2, e - bias) * (1 + f / Math.pow(2, fbits));
+      return s * pow(2, e - bias) * (1 + f / pow(2, fbits));
     } else if (f !== 0) {
       // Denormalized
-      return s * Math.pow(2, -(bias - 1)) * (f / Math.pow(2, fbits));
+      return s * pow(2, -(bias - 1)) * (f / pow(2, fbits));
     } else {
       return s < 0 ? -0 : 0;
     }
