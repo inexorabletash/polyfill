16b9cf934689bed4a1537f6aa19cc61aefe24f5b
Joshua Bell <inexorabletash@gmail.com>
2016-07-29 08:53:47 -0700

    Add ChildNode/ParentNode mixins to DOM

--- polyfill.js
+++ polyfill.js
@@ -4898,6 +4898,101 @@ function __cons(t, a) {
       };
     }
   }
+
+  function mixin(o, ps) {
+    Object.keys(ps).forEach(function(p) {
+      if ((p in o) || (p in o.prototype)) return;
+      Object.defineProperty(
+        o.prototype,
+        p,
+        Object.getOwnPropertyDescriptor(ps, p)
+      );
+    });
+  }
+
+  // Mixin ParentNode
+  // https://dom.spec.whatwg.org/#interface-parentnode
+
+  function convertNodesIntoANode(nodes) {
+    var node = null;
+    nodes = nodes.map(function(node) {
+      return !(node instanceof Node) ? document.createTextNode(node) : node;
+    });
+    if (nodes.length === 1) {
+      node = nodes[0];
+    } else {
+      node = document.createDocumentFragment();
+      nodes.forEach(function(n) { node.appendChild(n); });
+    }
+    return node;
+  }
+
+  var ParentNode = {
+    prepend: function(/*...nodes*/) {
+      var nodes = [].slice.call(arguments);
+      nodes = convertNodesIntoANode(nodes);
+      this.insertBefore(nodes, this.firstChild);
+    },
+    append: function(/*...nodes*/) {
+      var nodes = [].slice.call(arguments);
+      nodes = convertNodesIntoANode(nodes);
+      this.appendChild(nodes);
+    }
+  };
+
+  mixin(Document, ParentNode);
+  mixin(DocumentFragment, ParentNode);
+  mixin(Element, ParentNode);
+
+  // Mixin ChildNode
+  // https://dom.spec.whatwg.org/#interface-childnode
+
+  var ChildNode = {
+    before: function(/*...nodes*/) {
+      var nodes = [].slice.call(arguments);
+      var parent = this.parentNode;
+      if (!parent) return;
+      var viablePreviousSibling = this.previousSibling;
+      while (nodes.indexOf(viablePreviousSibling) !== -1)
+        viablePreviousSibling = viablePreviousSibling.previousSibling;
+      var node = convertNodesIntoANode(nodes);
+      parent.insertBefore(node, viablePreviousSibling ?
+                          viablePreviousSibling.nextSibling : parent.firstChild);
+    },
+    after: function(/*...nodes*/) {
+      var nodes = [].slice.call(arguments);
+      var parent = this.parentNode;
+      if (!parent) return;
+      var viableNextSibling = this.nextSibling;
+      while (nodes.indexOf(viableNextSibling) !== -1)
+        viableNextSibling = viableNextSibling.nextSibling;
+      var node = convertNodesIntoANode(nodes);
+      parent.insertBefore(node, viableNextSibling);
+    },
+    replaceWith: function(/*...nodes*/) {
+      var nodes = [].slice.call(arguments);
+      var parent = this.parentNode;
+      if (!parent) return;
+      var viableNextSibling = this.nextSibling;
+      while (nodes.indexOf(viableNextSibling) !== -1)
+        viableNextSibling = viableNextSibling.nextSibling;
+      var node = convertNodesIntoANode(nodes);
+
+      if (this.parentNode === parent)
+        parent.replaceChild(node, this);
+      else
+        parent.insertBefore(node, viableNextSibling);
+    },
+    remove: function() {
+      if (!this.parentNode) return;
+      this.parentNode.removeChild(this);
+    }
+  };
+
+  mixin(DocumentType, ChildNode);
+  mixin(Element, ChildNode);
+  mixin(CharacterData, ChildNode);
+
 }(self));
 (function(global) {
   if (!('window' in global && 'document' in global))
--- tests/dom_tests.js
+++ tests/dom_tests.js
@@ -174,3 +174,309 @@ test("matches", function() {
   assertTrue("document.querySelector('#foo').matches('.alpha')");
   assertTrue("document.querySelector('#baz').matches('.beta')");
 });
+
+test('Mixin ParentNode: prepend()', function() {
+  elem = document.querySelector('#mixin-parentnode');
+  var orig = elem.innerHTML;
+
+  assertEqual('elem.childNodes.length', 0);
+  assertEqual('elem.childElementCount', 0);
+  elem.prepend(document.createElement('span'));
+  assertEqual('elem.childNodes.length', 1);
+  assertEqual('elem.childElementCount', 1);
+  assertEqual('elem.childNodes[0].nodeType', Node.ELEMENT_NODE);
+
+  elem.prepend('text');
+  assertEqual('elem.childNodes.length', 2);
+  assertEqual('elem.childElementCount', 1);
+
+  assertEqual('elem.childNodes[0].nodeType', Node.TEXT_NODE);
+  assertEqual('elem.childNodes[1].nodeType', Node.ELEMENT_NODE);
+
+  elem.prepend('t2', document.createElement('span'), 't3');
+  assertEqual('elem.childNodes.length', 5);
+  assertEqual('elem.childElementCount', 2);
+
+  assertEqual('elem.childNodes[0].nodeType', Node.TEXT_NODE);
+  assertEqual('elem.childNodes[0].textContent', 't2');
+  assertEqual('elem.childNodes[1].nodeType', Node.ELEMENT_NODE);
+  assertEqual('elem.childNodes[2].nodeType', Node.TEXT_NODE);
+  assertEqual('elem.childNodes[2].textContent', 't3');
+  assertEqual('elem.childNodes[3].nodeType', Node.TEXT_NODE);
+  assertEqual('elem.childNodes[4].nodeType', Node.ELEMENT_NODE);
+
+  elem.innerHTML = orig;
+  delete elem;
+});
+
+test('Mixin ParentNode: append()', function() {
+  elem = document.querySelector('#mixin-parentnode');
+  var orig = elem.innerHTML;
+
+  assertEqual('elem.childNodes.length', 0);
+  assertEqual('elem.childElementCount', 0);
+  elem.append(document.createElement('span'));
+  assertEqual('elem.childNodes.length', 1);
+  assertEqual('elem.childElementCount', 1);
+  assertEqual('elem.childNodes[0].nodeType', Node.ELEMENT_NODE);
+
+  elem.append('text');
+  assertEqual('elem.childNodes.length', 2);
+  assertEqual('elem.childElementCount', 1);
+
+  assertEqual('elem.childNodes[0].nodeType', Node.ELEMENT_NODE);
+  assertEqual('elem.childNodes[1].nodeType', Node.TEXT_NODE);
+
+  elem.append('t2', document.createElement('span'), 't3');
+  assertEqual('elem.childNodes.length', 5);
+  assertEqual('elem.childElementCount', 2);
+
+  assertEqual('elem.childNodes[0].nodeType', Node.ELEMENT_NODE);
+  assertEqual('elem.childNodes[1].nodeType', Node.TEXT_NODE);
+  assertEqual('elem.childNodes[2].nodeType', Node.TEXT_NODE);
+  assertEqual('elem.childNodes[2].textContent', 't2');
+  assertEqual('elem.childNodes[3].nodeType', Node.ELEMENT_NODE);
+  assertEqual('elem.childNodes[4].nodeType', Node.TEXT_NODE);
+  assertEqual('elem.childNodes[4].textContent', 't3');
+
+  elem.innerHTML = orig;
+  delete elem;
+});
+
+test('Mixin ChildNode: before()', function() {
+  parent = document.querySelector('#mixin-childnode');
+  var orig = parent.innerHTML;
+  child = document.querySelector('#mixin-childnode *');
+
+  assertEqual('parent.childNodes.length', 1);
+  child.before(document.createElement('span'));
+  assertEqual('parent.childNodes.length', 2);
+  assertEqual('parent.childNodes[1]', child);
+
+  child.before('t1', document.createElement('span'), 't2');
+  assertEqual('parent.childNodes.length', 5);
+  assertEqual('parent.childNodes[0].nodeType', Node.ELEMENT_NODE);
+  assertEqual('parent.childNodes[1].nodeType', Node.TEXT_NODE);
+  assertEqual('parent.childNodes[1].textContent', 't1');
+  assertEqual('parent.childNodes[2].nodeType', Node.ELEMENT_NODE);
+  assertEqual('parent.childNodes[3].nodeType', Node.TEXT_NODE);
+  assertEqual('parent.childNodes[3].textContent', 't2');
+  assertEqual('parent.childNodes[4].nodeType', Node.ELEMENT_NODE);
+  assertEqual('parent.childNodes[4]', child);
+
+  parent.innerHTML = orig;
+  delete parent;
+  delete child;
+});
+
+test('Mixin ChildNode: after()', function() {
+  parent = document.querySelector('#mixin-childnode');
+  var orig = parent.innerHTML;
+  child = document.querySelector('#mixin-childnode *');
+
+  assertEqual('parent.childNodes.length', 1);
+  child.after(document.createElement('span'));
+  assertEqual('parent.childNodes.length', 2);
+  assertEqual('parent.childNodes[0]', child);
+
+  child.after('t1', document.createElement('span'), 't2');
+  assertEqual('parent.childNodes.length', 5);
+  assertEqual('parent.childNodes[0].nodeType', Node.ELEMENT_NODE);
+  assertEqual('parent.childNodes[0]', child);
+  assertEqual('parent.childNodes[1].nodeType', Node.TEXT_NODE);
+  assertEqual('parent.childNodes[1].textContent', 't1');
+  assertEqual('parent.childNodes[2].nodeType', Node.ELEMENT_NODE);
+  assertEqual('parent.childNodes[3].nodeType', Node.TEXT_NODE);
+  assertEqual('parent.childNodes[3].textContent', 't2');
+  assertEqual('parent.childNodes[4].nodeType', Node.ELEMENT_NODE);
+
+  parent.innerHTML = orig;
+  delete parent;
+  delete child;
+});
+
+test('Mixin ChildNode: replaceWith()', function() {
+  parent = document.querySelector('#mixin-childnode');
+  var orig = parent.innerHTML;
+  child = document.querySelector('#mixin-childnode *');
+
+  assertEqual('parent.childNodes.length', 1);
+  assertEqual('parent.childNodes[0]', child);
+  child.replaceWith(document.createElement('span'));
+  assertEqual('parent.childNodes.length', 1);
+  assertFalse('parent.childNodes[0] === child');
+  assertEqual('child.parentNode', null);
+
+  parent.innerHTML = orig;
+  child = document.querySelector('#mixin-childnode *');
+
+  assertEqual('parent.childNodes.length', 1);
+  assertEqual('parent.childNodes[0]', child);
+  child.replaceWith('t1', document.createElement('span'), 't2');
+  assertEqual('parent.childNodes.length', 3);
+  assertEqual('parent.childNodes[0].nodeType', Node.TEXT_NODE);
+  assertEqual('parent.childNodes[0].textContent', 't1');
+  assertEqual('parent.childNodes[1].nodeType', Node.ELEMENT_NODE);
+  assertEqual('parent.childNodes[2].nodeType', Node.TEXT_NODE);
+  assertEqual('parent.childNodes[2].textContent', 't2');
+  assertEqual('child.parentNode', null);
+
+  parent.innerHTML = orig;
+  delete parent;
+  delete child;
+});
+
+test('Mixin ChildNode: remove()', function() {
+  parent = document.querySelector('#mixin-childnode');
+  var orig = parent.innerHTML;
+  child = document.querySelector('#mixin-childnode *');
+
+  assertEqual('parent.childNodes.length', 1);
+  assertEqual('parent.childNodes[0]', child);
+  child.remove(document.createElement('span'));
+  assertEqual('parent.childNodes.length', 0);
+  assertEqual('child.parentNode', null);
+
+  parent.innerHTML = orig;
+  delete parent;
+  delete child;
+});
+
+test('Mixin ParentNode: More prepend(), append()', function() {
+  var div = document.createElement('div');
+  equal(div.textContent, '', 'div should start empty');
+
+  var span = document.createElement('span');
+  equal(span.textContent, '', 'span should start empty');
+  span.append('s');
+  equal(span.textContent, 's', 'span should have inserted text');
+
+  div.prepend('1', span);
+  equal(div.childNodes.length, 2, 'div should have only two children inserted');
+  equal(div.textContent, '1s', 'div should have two children inserted');
+
+  div.append(span, '2');
+  equal(div.childNodes.length, 3, 'div should have only one new child inserted');
+  equal(div.textContent, '1s2', 'div should have only one new child inserted');
+
+  span.before('a', 'b');
+  equal(div.childNodes.length, 5, 'div should have two new children inserted');
+  equal(div.textContent, '1abs2', 'div should have two new children inserted');
+
+  span.after('e', 'f');
+  equal(div.childNodes.length, 7, 'div should have two new children inserted');
+  equal(div.textContent, '1absef2', 'div should have two new children inserted');
+
+  span.replaceWith('c', 'd');
+  equal(div.childNodes.length, 8, 'replaceWith() should replace target with two nodes');
+  equal(div.textContent, '1abcdef2', 'replaceWith() should replace target with two nodes');
+});
+
+test('Mixin ChildNode: More before(), after()', function() {
+  var div = document.createElement('div');
+  var span = document.createElement('span');
+  span.append('s');
+  div.append(span);
+  span.after('1');
+  equal(div.textContent, 's1', 'after() should work on last child');
+  span.after('2');
+  equal(div.textContent, 's21', 'after() should work on not last child');
+  span.before('3');
+  equal(div.textContent, '3s21', 'before() should work on first child');
+  span.before('4');
+  equal(div.textContent, '34s21', 'before() should work on not first child');
+});
+
+test('Mixin ChildNode: More remove()', function() {
+  var div = document.createElement('div');
+  var span = document.createElement('span');
+  span.append('s');
+  div.append('a', span, 'b');
+  equal(div.textContent, 'asb');
+  span.remove();
+  equal(div.textContent, 'ab', 'remove() should remove child');
+
+  div = document.createElement('div');
+  div.append(span, 'a', 'b');
+  equal(div.textContent, 'sab');
+  span.remove();
+  equal(div.textContent, 'ab', 'remove() should remove child if first');
+
+  div = document.createElement('div');
+  div.append('a', 'b', span);
+  equal(div.textContent, 'abs');
+  span.remove();
+  equal(div.textContent, 'ab', 'remove() should remove child if last');
+});
+
+test('Mixin ChildNode: remove() - Behavior when no parent node', function() {
+  var div = document.createElement('div');
+  var span = document.createElement('span');
+  equal(div.parentNode, null);
+  equal(span.parentNode, null);
+
+  div.before(span);
+  equal(span.parentNode, null);
+
+  div.after(span);
+  equal(span.parentNode, null);
+
+  div.replaceWith(span);
+  equal(span.parentNode, null);
+
+  div.remove(); // Shouldn't throw
+});
+
+test('Mixin ChildNode: prepend, append, replaceWith - Behavior with no arguments', function() {
+  var div = document.createElement('div');
+  var span = document.createElement('span');
+  div.prepend();
+  div.append();
+
+  div.append(span);
+  span.before();
+  span.after();
+  equal(div.childNodes.length, 1);
+  equal(span.parentNode, div);
+  span.replaceWith();
+  equal(div.childNodes.length, 0);
+  equal(span.parentNode, null);
+});
+
+test('Mixin ChildNode: before, after, replaceWith - Behavior when nodes contains context node or siblings', function() {
+  function span(t) {
+    var s = document.createElement('span');
+    s.append(t);
+    return s;
+  }
+  var s1 = span('1');
+  var s2 = span('2');
+  var s3 = span('3');
+  var s4 = span('4');
+
+  var div = document.createElement('div');
+  function reset() {
+    div.append(s1, s2, s3, s4);
+    equal(div.textContent, '1234');
+  }
+
+  reset();
+  s2.replaceWith(s3, s2);
+  equal(div.textContent, '1324', 'replaceWith should handle replacing self');
+
+  reset();
+  s1.before(s3, s2, s1);
+  equal(div.textContent, '3214', 'before should handle replacing self');
+
+  reset();
+  s2.before(s3, s2, s1);
+  equal(div.textContent, '3214', 'before should work when prior is removed');
+
+  reset();
+  s4.after(s4, s3, s2);
+  equal(div.textContent, '1432', 'after should handle replacing self');
+
+  reset();
+  s3.after(s4, s3, s2);
+  equal(div.textContent, '1432', 'after should work when successor is removed');
+});
