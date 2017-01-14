78ccdc7ac54e3ba0251264b3d1a871f71a161bc0
Joshua Bell <inexorabletash@gmail.com>
2017-01-13 20:02:17 -0800

    DOM: Simplify CustomEvent polyfill

--- dom.js
+++ dom.js
@@ -217,16 +217,15 @@
   // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
   // Needed for: IE
   (function () {
-    if ('CustomEvent' in global) {
-      if ( typeof global.CustomEvent === "function" ) return false;
-    }
+    if ('CustomEvent' in global && typeof global.CustomEvent === "function")
+      return;
     function CustomEvent ( event, params ) {
       params = params || { bubbles: false, cancelable: false, detail: undefined };
       var evt = document.createEvent( 'CustomEvent' );
       evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
       return evt;
     }
-    CustomEvent.prototype = window.Event.prototype;
+    CustomEvent.prototype = global.Event.prototype;
     global.CustomEvent = CustomEvent;
   })();
 
