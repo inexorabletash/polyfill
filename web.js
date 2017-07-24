f763c09fbd57d43703fe71d5b2719de3412e297e
Joshua Bell <inexorabletash@gmail.com>
2017-07-24 08:35:34 -0700

    Update bundled/minified files

--- web.js
+++ web.js
@@ -695,6 +695,21 @@
     }
   }
 
+  // Element.closest
+  // https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+  if (window.Element && !Element.prototype.closest) {
+    Element.prototype.closest = function(s) {
+      var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+          i,
+          el = this;
+      do {
+        i = matches.length;
+        while (--i >= 0 && matches.item(i) !== el) {};
+      } while ((i < 0) && (el = el.parentElement)); 
+      return el;
+    };
+  }
+
   function mixin(o, ps) {
     if (!o) return;
     Object.keys(ps).forEach(function(p) {
